c295d8548b3b5fa723d5d91bffce8360cb541b4e
regiakb <albert@reginaldo.cat>
2022-06-16 18:46:51 +0200

    Update screenshot.bat

--- screenshot.bat
+++ screenshot.bat
@@ -3,6 +3,6 @@ setlocal
 set counter=1
 :repeat
 ping localhost  -n 4 -w 60
-nircmd.exe savescreenshot "C:\Users\Regi\Desktop\screencapture\screen%counter%.png"
+nircmd.exe savescreenshot "C:\screencapture\screen%counter%.png"
 set /a counter=counter+1
 goto :repeat
