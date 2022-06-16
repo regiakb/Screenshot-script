bdae67204fc309fd256ad0d673ab04eceaf1bc72
regiakb <albert@reginaldo.cat>
2022-06-16 18:46:00 +0200

    Add files via upload

---
+++ screenshot.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+set counter=1
+:repeat
+ping localhost  -n 4 -w 60
+nircmd.exe savescreenshot "C:\Users\Regi\Desktop\screencapture\screen%counter%.png"
+set /a counter=counter+1
+goto :repeat
